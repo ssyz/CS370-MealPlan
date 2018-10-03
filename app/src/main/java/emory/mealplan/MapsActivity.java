140a6706fad5349bd32cc339c09d3b1bf4c29c55
njaffe2015 <43552140+njaffe2015@users.noreply.github.com>
2018-10-02 21:47:41 -0400

    updated with emory center

--- app/src/main/java/emory/mealplan/MapsActivity.java
+++ app/src/main/java/emory/mealplan/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng emory = new LatLng(33.7925, -84.3240);
+        mMap.addMarker(new MarkerOptions().position(emory).title("Emory University"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(emory));
     }
 }
