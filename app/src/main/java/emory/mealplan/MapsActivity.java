e03661793addd324c2b131030c350b6f15462663
njaffe2015 <43552140+njaffe2015@users.noreply.github.com>
2018-11-11 15:43:23 -0500

    bumped to ashwin's vers

--- app/src/main/java/emory/mealplan/MapsActivity.java
+++ app/src/main/java/emory/mealplan/MapsActivity.java
@@ -1,21 +1,27 @@
 package emory.mealplan;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
-import com.google.android.gms.location.LocationListener;
+// import android.location.LocationListener;
 
 import com.google.android.gms.location.LocationListener;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
@@ -28,17 +34,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 import java.io.InputStream;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
-    protected LocationManager locationManager;
-    protected LocationListener locationListener;
-    protected Context context;
-    private Location location;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,LocationListener, GoogleMap.OnMarkerClickListener {
     private GoogleMap mMap;
     private GoogleApiClient googleApiClient;
     private LocationRequest locationRequest;
@@ -54,23 +58,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            checkUserLocationPermission();
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-<<<<<<< HEAD
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+
+
+
+         Button infoButton;
+
+        infoButton= (Button) findViewById(R.id.viewinfo);
+        infoButton.setEnabled(true);
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openInfo();
+
+            }
+        });
+
+
 
     }
 
@@ -87,35 +100,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
+
         UiSettings mapUI= mMap.getUiSettings();
         mapUI.setZoomControlsEnabled(true);
         InputStream inputStream = getResources().openRawResource(R.raw.garestaurantpois);
         CSVReadNoah csvFile = new CSVReadNoah(inputStream);
         List poiList = csvFile.read();
-        for(int i=2; i<poiList.size()-1;i=i+2){
+        for(int i=3; i<poiList.size()-2;i=i+3){
             Object row=poiList.get(i);
             LatLng newMarker = new LatLng(Double.parseDouble(poiList.get(i+1).toString()),Double.parseDouble(poiList.get(i).toString()));
-            mMap.addMarker(new MarkerOptions().position(newMarker).alpha(1f));
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.title(poiList.get(i+2).toString());
+            markerOptions.position(newMarker);
+
+            mMap.addMarker(markerOptions);
         }
-        CheckUserLocationPermission();
-        MarkerOptions markerOptions = new MarkerOptions();
-        LatLng emory = new LatLng(33.7925, -84.3240);
-        markerOptions.position(emory);
-        markerOptions.title("Emory University");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-        mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(emory,15));
-        Circle circle = mMap.addCircle(new CircleOptions().center(emory).radius(1000).strokeColor(Color.BLACK).fillColor(Color.parseColor("#2271cce7")).strokeWidth(0));
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
             buildGoogleApiClient();
 
             mMap.setMyLocationEnabled(true);
-        }
-
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation, 15);
-        //MarkerOptions markerOptions = new MarkerOptions();
-       // LatLng emory = new LatLng(33.7925, -84.3240);
 
         }
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation, 15);
@@ -125,61 +130,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //markerOptions.title("Emory University");
         //markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
         //mMap.addMarker(markerOptions);
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(emory,15));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(emory,15));
         //Circle circle = mMap.addCircle(new CircleOptions().center(emory).radius(1000).strokeColor(Color.BLACK).fillColor(Color.parseColor("#2271cce7")).strokeWidth(0));
     }
+    public boolean onMarkerClick(Marker marker){
+        Location self=new Location("");
+        self.setLatitude(currentUserLocationMarker.getPosition().latitude);
+        self.setLongitude(currentUserLocationMarker.getPosition().longitude);
+        Location marked=new Location("");
+        marked.setLongitude((marker.getPosition().longitude));
+        marked.setLatitude((marker.getPosition().latitude));
 
-    public boolean CheckUserLocationPermission()
-    {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }else{
+        float distance=self.distanceTo(marked);
+        marker.setSnippet("Distance from current location: "+distance+" meters");
 
-            //Write you code here if permission already given.
-=======
-    public boolean checkUserLocationPermission()
-    {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
-            {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
-            }
-            else
-            {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
-            }
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+
+
+
+        return false;
+
+
+
     }
 
+    public void openInfo(){
+        Intent intent = new Intent(this, RestaurantInfoView.class);
+        startActivity(intent);
+
+
+    }
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        switch (requestCode)
+    public void onLocationChanged(Location location) {
+        latitide = location.getLatitude();
+        longitude = location.getLongitude();
+
+        lastLocation = location;
+
+        if (currentUserLocationMarker != null)
         {
-            case Request_User_Location_Code:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                    {
-                        if (googleApiClient == null)
-                        {
-                            buildGoogleApiClient();
-                        }
-                        mMap.setMyLocationEnabled(true);
-                    }
-                }
-                else
-                {
-                    Toast.makeText(this, "Permission Denied...", Toast.LENGTH_SHORT).show();
-                }
-                return;
+            currentUserLocationMarker.remove();
+        }
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("user Current Location");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        currentUserLocationMarker = mMap.addMarker(markerOptions);
+
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
@@ -214,47 +214,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-
-
-
-    protected synchronized void buildGoogleApiClient()
-    {
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-
-        googleApiClient.connect();
-    }
     @Override
-    public void onLocationChanged(Location location) {
-        latitide = location.getLatitude();
-        longitude = location.getLongitude();
-
-        lastLocation = location;
-
-        if (currentUserLocationMarker != null)
-        {
-            currentUserLocationMarker.remove();
-        }
-
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("user Current Location");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-
-        currentUserLocationMarker = mMap.addMarker(markerOptions);
-
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
-        Circle circle = mMap.addCircle(new CircleOptions().center(latLng).radius(1000).strokeColor(Color.BLACK).fillColor(Color.parseColor("#2271cce7")).strokeWidth(0));
-        if (googleApiClient != null)
-        {
-            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
-        }
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
     {
         switch (requestCode)
@@ -277,11 +237,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
                 return;
         }
-        return false;
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
 
 
 
@@ -296,18 +253,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         googleApiClient.connect();
     }
 
+
     @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
+    public void onConnected(@Nullable Bundle bundle)
+    {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(11000);
+        locationRequest.setFastestInterval(1100);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
+
 
     }
 
     @Override
-    public void onProviderEnabled(String s) {
+    public void onConnectionSuspended(int i) {
 
     }
 
     @Override
-    public void onProviderDisabled(String s) {
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
+
